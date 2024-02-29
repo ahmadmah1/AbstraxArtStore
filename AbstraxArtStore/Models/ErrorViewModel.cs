a68ea152cffbad110502d06dce2cadfc8303b8b6
ac117029 <ac117029@8CG8163L15>
2024-02-29 15:03:13 +1300

    Add project files.

---
+++ AbstraxArtStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AbstraxArtStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
