b38eae3eb7d211a8e2f5543219ab52dbde179980
Ahmad Mahmoud <ac117029@avcol.school.nz>
2024-03-24 18:47:56 +1300

    links

--- AbstraxArtStore/Controllers/HomeController.cs
+++ AbstraxArtStore/Controllers/HomeController.cs
@@ -23,6 +23,26 @@ namespace AbstraxArtStore.Controllers
             return View();
         }
 
+        public IActionResult Buy()
+        {
+            return View();
+        }
+
+        public IActionResult Sell()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
